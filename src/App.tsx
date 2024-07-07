9ac2aed52faf5f91d16c04112c3f0ab807043c77
AlexAdleyba <light_lex@mail.ru>
2024-07-07 17:14:55 +0300

    Commit first

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ function App() {
 
     const tasks1 : Task[] = [
         {id: 1, title: 'HTML&CSS', isDone: true},
-        {id: 2, title: 'JS', isDone: true},
+        {id: 2, title: 'JS&TS', isDone: true},
         {id: 3, title: 'ReactJS', isDone: false},
     ]
 
